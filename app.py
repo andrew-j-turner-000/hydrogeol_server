eca4f7b65ff568eeda29b86270eec5c478a3d2fe
Andrew Turner <andrew.turner@ga.gov.au>
2019-04-15 11:42:25 +1000

    Added wsgi file and link to jupyter notebook

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def test_conn():
 
 @app.route('/jupyter')
 def trigger_jupyter_notebooks():
-    subprocess.run('[source, /var/www/hydrogeol_server/venv/bin/activate]')
+    subprocess.run('source /var/www/hydrogeol_server/venv/bin/activate]')
     result = subprocess.run('jupyter notebook', stdout=subprocess.PIPE)
     print(result.stdout)
     return render_template('hello.html', test=result
